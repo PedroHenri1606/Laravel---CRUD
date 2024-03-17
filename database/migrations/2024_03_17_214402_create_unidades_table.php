aaa480a505ac7efdb4c3494c3f29cb99e611a08e
Pedro Henrique <pedrohenri1606@gmail.com>
2024-03-17 19:13:10 -0300

    Criando relacionamento OneToMany entre tabelas

---
+++ database/migrations/2024_03_17_214402_create_unidades_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('unidades', function (Blueprint $table) {
+            $table->id();
+            $table->string('unidade', 5);
+            $table->string('descricao', 30);
+            $table->timestamps();
+        });
+
+        //adicionar o relacionamento com a tabela produtos
+        Schema::table('produtos', function(Blueprint $table){
+            $table->unsignedBigInteger('unidade_id');  //Criação de uma nova coluna na tabela produtos 
+            $table->foreign('unidade_id')->references('id')->on('unidades'); //Informa que a coluna recem criada, referenciara o id da tabela unidades
+        });
+
+        //adicionar o relacionamento com a tabela produtos_detalhes
+        Schema::table('produto_detalhes', function(Blueprint $table){
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreign('unidade_id')->references('id')->on('unidades');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //Remover o relacionamento com a tabela produtos
+        Schema::table('produtos', function(Blueprint $table){
+            //Remover a fk
+            $table->dropForeign('produtos_unidade_id_foreign)');
+            //Remover a coluna
+            $table->dropColumn('unidade_id');
+        });
+
+        //Remover o relacionamento com a tabela produtos_detalhes
+        Schema::table('produto_detalhes', function(Blueprint $table){
+            //Remover a fk
+            $table->dropForeign('produtos_detalhes_unidade_id_foreign)');
+            //Remover a coluna
+            $table->dropColumn('unidade_id');
+        });
+
+        Schema::dropIfExists('unidades');
+    }
+};
