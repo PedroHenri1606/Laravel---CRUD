eafe9d8de036a4a16ec66579403c9b5bc3b47a64
Pedro Henrique <pedrohenri1606@gmail.com>
2024-03-18 14:54:24 -0300

    Conclusão estudo ORM, Inicio estudo Seeders

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -4,12 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     //Correção do Eloquent ORM
     protected $table = 'fornecedores';
     protected $fillable = ['nome','site','uf','email'];  //Variavel que faz a autorização de manipulação das variaveis pelo tinker. 
+
+     
 }
