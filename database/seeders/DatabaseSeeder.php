fd21a4a88c81df2be022cdc85a2f2d1326186608
Pedro Henrique <pedrohenri1606@gmail.com>
2024-03-18 15:30:24 -0300

    Efetuado a criação e configuração das Seeders, iniciando estudo de Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        
+        $this->call(
+            [
+                FornecedorSeeder::class,
+                SiteContatoSeeder::class
+            ]);
 
+        /*
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        */
     }
 }
--- database/seeders/FornecedorSeeder.php
+++ database/seeders/FornecedorSeeder.php
@@ -4,14 +4,41 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Fornecedor;
+use Illuminate\Support\Facades\DB;
 
 class FornecedorSeeder extends Seeder
+
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        //estilo 1 de criação - instanciação do objeto
+        $fornecedor = new Fornecedor();
+        $fornecedor->nome = 'Fornecedor 100';
+        $fornecedor->email = 'fornecedor100@gmail.com';
+        $fornecedor->uf = 'CE';
+        $fornecedor->site = 'contato@fornecedor100.com.br';
+        $fornecedor->save();
+
+        //estilo 2 de criação - metodo create (cuidado com o fillable)
+        Fornecedor::create(
+            [
+                'nome'  => 'Fornecedor101',
+                'site'  => 'fornecedor101.com.br',
+                'uf'    => 'PR',
+                'email' => 'fornecedor101@gmail.com',
+            ]);
+
+        //estilo 3 de criação - metodo insert
+        DB::table('fornecedores')->insert(
+            [
+                'nome'  => 'Fornecedor102',
+                'site'  => 'fornecedor102.com.br',
+                'uf'    => 'SP',
+                'email' => 'fornecedor102@gmail.com',
+            ]);
     }
 }
