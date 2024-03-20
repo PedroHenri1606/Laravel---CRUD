daf11e29306af358e9760cd7f453a75bff38a997
Pedro Henrique <pedrohenri1606@gmail.com>
2024-03-20 17:56:31 -0300

    Alterando a resposta da requisição via middleware

--- app/Http/Middleware/LogAcessoMiddleware.php
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -25,6 +25,11 @@ class LogAcessoMiddleware
         $ip = $request->server->get("REMOTE_ADDR");
         $rote = $request->getRequestUri();
         LogAcesso::create(['log' => "$ip requisitou a rota $rote"]);
-        return $next($request);
+
+        $resposta = $next($request);
+
+        $resposta->setStatusCode(201,'O status da resposta e o texto da resposta foram modificados!');
+
+        return $resposta;
     }
 }
