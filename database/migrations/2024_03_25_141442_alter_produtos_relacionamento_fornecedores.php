c1af58e512560e9fbcbcffec71a6b28645e38ffd
Pedro Henrique <pedrohenri1606@gmail.com>
2024-03-25 11:39:57 -0300

    Criando relacionamento Produto x Fornecedor

--- database/migrations/2024_03_25_141442_alter_produtos_relacionamento_fornecedores.php
+++ database/migrations/2024_03_25_141442_alter_produtos_relacionamento_fornecedores.php
@@ -11,7 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        //Criando a relação produto com fornecedores
+        Schema::table('produtos', function(Blueprint $table){
+
+            $fornecedor_id = DB::table('fornecedores')->insertGetId([
+                'nome' => 'Fornecedor Padrão SG',
+                'site' => 'fornecedorpadraosg.com.br',
+                'uf' => 'PR',
+                'email' => 'contato@fornecedorsg.com.br  ',
+            ]);
+
+            $table->unsignedBigInteger('fornecedor_id')->default($fornecedor_id)->after('id');
+
+            $table->foreign('fornecedor_id')->references('id')->on('fornecedores');
+            //Sobre a foreign key fornecedores_id, ela referecia a coluna id, na tabela fornecedores
+        });
     }
 
     /**
@@ -19,6 +33,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+
+        Schema::table('produtos', function(Blueprint $table){
+            $table->dropForeign('produtos_fornecedor_id_foreign');
+            $table->dropColum('forncedor_id');
+        });
     }
 };
